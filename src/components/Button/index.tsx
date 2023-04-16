53e072ba9a61345d7bb14c107d831534a4060c33
Mauricio Brito <maubot01@gmail.com>
2023-04-16 18:29:00 -0300

    feat: button variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,28 +1,89 @@
+import { Icon, IconType, IconSize } from '@/components';
+import { AssetPosition } from '@/types';
+
+export enum ButtonVariant {
+  PRIMARY = 'PRIMARY',
+  ICON = 'ICON',
+}
+
 export interface IButtonProps {
-  label: string;
+  label?: string;
   handleClick?: () => void;
   labelClassName?: string;
   className?: string;
+  ariaLabel?: string;
+  iconType?: IconType;
+  iconSize?: IconSize;
+  iconPosition?: AssetPosition;
+  iconClassName?: string;
+  variant?: ButtonVariant;
 }
+
+type VariantCssClasses = {
+  container?: string;
+  label?: string;
+};
+
+const ButtonCssClasses: Record<ButtonVariant, VariantCssClasses> = {
+  [ButtonVariant.PRIMARY]: {
+    container:
+      'bg-gradient-to-r from-primary to-primary-dark drop-shadow-lg inline-flex items-center' +
+      ' justify-center text-white rounded-2xl h-14 w-full px-2 hover:bg-primary-dark transition duration-300',
+    label: 'font-bold ',
+  },
+  [ButtonVariant.ICON]: {
+    container:
+      'flex items-center justify-center w-10 h-10 bg-black opacity-50 ' +
+      'p-2 rounded-full transition duration-300 hover:opacity-100',
+  },
+};
+
+const getButtonCssClasses = (
+  variant: ButtonVariant,
+  key: keyof VariantCssClasses
+): string => ButtonCssClasses[variant][key] ?? '';
+
+export const iconPositionClasses: Record<AssetPosition, string> = {
+  [AssetPosition.LEFT]: '-order-1 mr-3',
+  [AssetPosition.RIGHT]: 'ml-3',
+  [AssetPosition.CENTER]: 'm-0',
+};
 
 export const Button = ({
   label,
   labelClassName,
   handleClick,
   className,
+  ariaLabel,
+  iconType,
+  iconSize,
+  iconClassName,
+  iconPosition,
+  variant = ButtonVariant.PRIMARY,
 }: IButtonProps) => {
   return (
     <button
       className={`
-        bg-gradient-to-r from-primary to-primary-dark drop-shadow-lg inline-flex items-center justify-center
-        text-white rounded-2xl h-14 w-full px-2 hover:bg-primary-dark transition duration-300 ${
-          className ?? ''
-        }`}
+        cursor-pointer
+        ${getButtonCssClasses(variant, 'container')}
+        ${iconType && iconPosition ? iconPositionClasses[iconPosition] : ''} 
+        ${className ?? ''}
+      `}
       onClick={handleClick}
+      aria-label={ariaLabel ?? ''}
     >
-      {label ? (
-        <span className={`font-bold ${labelClassName ?? ''}`}>{label}</span>
-      ) : null}
+      {label && (
+        <span
+          className={`${getButtonCssClasses(variant, 'label')}  ${
+            labelClassName ?? ''
+          }`}
+        >
+          {label}
+        </span>
+      )}
+      {iconType && (
+        <Icon className={iconClassName ?? ''} type={iconType} size={iconSize} />
+      )}
     </button>
   );
 };
