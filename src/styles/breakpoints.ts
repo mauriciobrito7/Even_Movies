2e94f93dcfd74017ecc41260e0c73a4c01762aeb
Mauricio Brito <maubot01@gmail.com>
2023-04-17 02:15:28 -0300

    feat: useWindowWidth added

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,17 @@
+export interface IBreakpoints {
+  xxxldesktop: number;
+  xxldesktop: number;
+  xldesktop: number;
+  desktop: number;
+  tablet: number;
+  mobile: number;
+}
+
+export const breakpoints: IBreakpoints = {
+  xxxldesktop: 1920,
+  xxldesktop: 1440,
+  xldesktop: 1280,
+  desktop: 1024,
+  tablet: 720,
+  mobile: 320,
+};
