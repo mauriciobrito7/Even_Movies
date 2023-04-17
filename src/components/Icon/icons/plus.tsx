b140943b2cb110106a1b83bb44976b0bf4a0f296
Mauricio Brito <maubot01@gmail.com>
2023-04-17 13:27:33 -0300

    fix: icons added

---
+++ src/components/Icon/icons/plus.tsx
@@ -0,0 +1,15 @@
+export const Plus = (
+  props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
+) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    data-icon="plus"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M13 12h5.5a.5.5 0 0 1 0 1H13v5.5a.5.5 0 0 1-1 0V13H6.5a.5.5 0 0 1 0-1H12V6.5a.5.5 0 0 1 1 0V12Z"
+    />
+  </svg>
+);
